40141f2568b9274cb57c4b8e5cd2865c0d8389bc
Shahid Ghafoor <shgr8rb@gmail.com>
2017-10-28 14:26:20 +0500

    add language

--- app/src/main/java/com/byteshaft/hotelbooking/AppGlobals.java
+++ app/src/main/java/com/byteshaft/hotelbooking/AppGlobals.java
@@ -1,19 +1,21 @@
 package com.byteshaft.hotelbooking;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-/**
- * Created by s9iper1 on 10/27/17.
- */
+import java.util.Locale;
 
 public class AppGlobals extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Locale.getDefault().getLanguage();
+        changeLang(getApplicationContext(), Locale.getDefault().getLanguage());
+        System.out.println(Locale.getDefault().getLanguage());
         FirebaseApp.initializeApp(getApplicationContext());
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
@@ -22,5 +24,13 @@ public class AppGlobals extends Application {
             }
         }, 3000);
 
+    }
+
+    public static void changeLang(Context context, String lang) {
+        Locale myLocale = new Locale(lang);
+        Locale.setDefault(myLocale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = myLocale;
+        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
     }
 }
